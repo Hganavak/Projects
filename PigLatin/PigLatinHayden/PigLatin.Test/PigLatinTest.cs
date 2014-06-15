028f19be6003918c73e2a6c4e66b8eb620e765d8
Hayden Harrison <hayden.j.harrison@gmail.com>
2014-06-16 08:56:57 +1200

    Did the PigLatin one complete with unit tests! :P

---
+++ PigLatin/PigLatinHayden/PigLatin.Test/PigLatinTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PigLatin.Test {
+    [TestClass]
+    public class PigLatinTest {
+        [TestMethod]
+        public void ReturnsCorrectOutputForFirstLetterConsonant(){
+            string word = "hayden";
+            string result = PigLatinizer.PigLatinify(word);
+            string expected = "aydenhay";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReturnsCorrectOutputForFirstLetterVowel(){
+            string word = "apple";
+            string result = PigLatinizer.PigLatinify(word);
+            string expected = "appleway";
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ PigLatin/PigLatinHayden/PigLatin/PigLatinizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin {
+    public class PigLatinizer {
+        public static List<string> vowels = new List<string>{"a","e","i","o","u"}; 
+        
+        public static string PigLatinify(string word){
+            word = word.ToLower();
+            return vowels.Contains(word.Substring(0, 1)) 
+                ? word + "way"
+                : word.Substring(1, word.Length - 1) + word.Substring(0, 1) + "ay";
+        }
+    }
+}
